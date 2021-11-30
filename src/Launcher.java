5feff34ddf85dda235d95168ed46fe54ba50118e
unknown <hochard@et.esiea.fr>
2021-11-30 09:37:58 +0100

    Part 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 class Launcher {
     public static void main(String[] args) {
@@ -13,19 +17,32 @@ class Launcher {
             if ("quit".equals(sc)) {
 
             }
-
             else if ("fibo".equals(sc)) {
                 System.out.println("Enter a number:");
 
                 int n = myObj.nextInt();
-                sc = myObj.nextLine();  // Read user input
+                myObj.nextLine();  // Read user input
+
                 int n1=0, n2=1, n3=0, i;
+
                 for (i = 0; i < n-1; ++i) {
                     n3=n1+n2;
                     n1=n2;
                     n2=n3;
                 }
                 System.out.println("F(" + n + ") = "+ n3);
+            }
+            else  if ("freq".equals(sc)) {
+                System.out.println("What is the path of the file you want to analyse?");
+                Path path = Paths.get(myObj.nextLine());
+
+                try {
+                    String file = Files.readString(path);
+                    System.out.println("Sorry, this function is not working.");
+
+                } catch (IOException e) {
+                    System.out.println("Unreadable File: " + e);
+                }
             }
             else {
                 System.out.println("Unknown command");
