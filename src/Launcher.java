67475604a3958f685d01e01c000509db4542d165
unknown <hochard@et.esiea.fr>
2021-11-30 09:02:26 +0100

    Part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,11 +5,31 @@ class Launcher {
         System.out.println("Welcome");
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        String sc = myObj.nextLine();  // Read user input
+        String sc = "";  // Read user input
 
         while (!"quit".equals(sc)) {
-            System.out.println("Unknown command");
             sc = myObj.nextLine();  // Read user input
+
+            if ("quit".equals(sc)) {
+
+            }
+
+            else if ("fibo".equals(sc)) {
+                System.out.println("Enter a number:");
+
+                int n = myObj.nextInt();
+                sc = myObj.nextLine();  // Read user input
+                int n1=0, n2=1, n3=0, i;
+                for (i = 0; i < n-1; ++i) {
+                    n3=n1+n2;
+                    n1=n2;
+                    n2=n3;
+                }
+                System.out.println("F(" + n + ") = "+ n3);
+            }
+            else {
+                System.out.println("Unknown command");
+            }
         }
     }
 }
